5e6ccce9a93c30acd8438d48f122ccfbd37dec40
luohao <APPLE@workdemacbook-pro.local>
2017-05-31 16:28:16 +0800

    Initial Commit

---
+++ mytest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mytest
+//
+//  Created by APPLE on 2017/5/31.
+//  Copyright © 2017年 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mytest
+//
+//  Created by APPLE on 2017/5/31.
+//  Copyright © 2017年 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
